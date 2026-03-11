09d02004dfae525e078c41ec4f07f4705640398a
Luke Williams <lukejwilliams6554@gmail.com>
2026-03-11 13:06:01 -0600

    refactor mart to use star schema joins from fct and dim tables

--- wildlife_lakehouse/models/marts/aggregations/mart_elk_sightings_state_agg.sql
+++ wildlife_lakehouse/models/marts/aggregations/mart_elk_sightings_state_agg.sql
@@ -1,20 +1,24 @@
 SELECT 
-sighting_date,
-season,
-state,
-sum(case when coordinate_quality = 'low'
+dd.sighting_date
+,fes.season
+,ds.state
+,sum(case when fes.coordinate_quality = 'low'
     then 1
     else 0
     end) as count_low_quality_sightings,
-sum(case when coordinate_quality = 'medium'
+sum(case when fes.coordinate_quality = 'medium'
     then 1
     else 0
     end) as count_medium_quality_sightings,
-sum(case when coordinate_quality = 'high'
+sum(case when fes.coordinate_quality = 'high'
     then 1
     else 0
     end) as count_high_quality_sightings,
-count(distinct(gbifID)) as sightings_count,
-AVG(coordinate_uncertainty_meters) as avg_coordinate_uncertainty
-FROM {{ref('int_elk_sightings')}}
+count(distinct(fes.gbifID)) as sightings_count
+FROM {{ref('fct_elk_sightings')}} fes
+    LEFT JOIN {{ref('dim_date')}} dd
+        ON fes.sighting_date = dd.sighting_date
+    LEFT JOIN {{ref('dim_location')}} ds
+        ON fes.county = ds.county
+            and fes.state = ds.state
 GROUP BY 1,2,3
